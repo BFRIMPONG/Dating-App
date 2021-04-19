20d33929b4c0f4a4c134474c834d4cf3377db068
BFRIMPONG <bismark.frimpong@amalitech.org>
2021-04-19 16:44:47 +0000

    UI updated

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ window.addEventListener('load', function() {
                            <div>' + users[i].name + '</div>\
                            <div>' + users[i].age + '</div>\
                            <div>' + users[i].hobby + '</div></div>\
-                            <button>Add as friend</button></div>';  
+                            <button id = "addFriend">Add as friend</button></div>';  
                     }
                 }
             }
